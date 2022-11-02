990b2b892312024257965444c7b36863bf4ed27c
Folasayo Samuel <folasayosamuel@gmail.com>
2022-11-02 12:20:44 +0100

    update

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,1 +1,13 @@
 // WHAT IS A MODEL? A model is a set of classes that represent the data that the app manages
+
+using System;
+
+namespace TodoApi.Models
+{
+	public class TodoItem
+	{
+		public long Id { get; set; }
+		public string? Name{ get; set; }
+		public bool IsComplete { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// In ASP.NET Core, services such as the DB context must be registered with the dependency injection (DI) container. The container provides the service to controllers.
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
